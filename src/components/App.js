b6f7534b2389b57bbe026802c94cd85aaa930570
Steve Golden <sgolden2525@outlook.com>
2019-10-14 08:59:51 -0500

    Refactoring components
    Added components folder
    Added components files within folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import marked from 'marked';
 import DOMPurify from 'dompurify';
-import './App.css';
+import '../App.css';
 
 class App extends React.Component {
   constructor(props) {
